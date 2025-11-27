73902a9167de225b9c5ee16a040d9f01896363e6
Aniketnavele05 <aniketnavele@gmail.com>
2025-11-27 16:53:54 +0530

    _

--- Rock/Room/urls.py
+++ Rock/Room/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 from .views import Registration
+from rest_framework_simplejwt.views import TokenObtainPairView ,TokenRefreshView
 
 urlpatterns = [
     path('',views.index,name='home'),
     path('api/register/',Registration.as_view(),name='registration'),
+    path('login/',TokenObtainPairView.as_view(),name='login'),
+    path('refresh/',TokenRefreshView.as_view(),name='token_refresh'),
 ]
