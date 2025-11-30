fa2fc686b8903da66e028a1e95aaadb98130b2ad
Aniketnavele05 <aniketnavele@gmail.com>
2025-11-30 17:40:50 +0530

    Room CRUD

--- Rock/Room/views.py
+++ Rock/Room/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import User
-from .serializer import RegistrationSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import User, Room
+from .serializer import RegistrationSerializer, RoomCreateSerializer, RoomJoinSerializer, RoomLeaveSerializer, RoomSerializer
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -14,3 +15,49 @@ class Registration(APIView):
             serializer.save()
             return redirect('/')
         return Response(serializer.errors, status=400)
+    
+class CreateRoom(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        serializer = RoomCreateSerializer(data={},context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        room = serializer.save()
+
+        request.user.current_room = room
+        request.user.save()
+
+        return Response(RoomCreateSerializer(room).data,status=202)
+
+class JoinRoom(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        serializer = RoomJoinSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        code = serializer.validated_data['room_code']
+        room = Room.objects.get(room_code = code)
+
+        request.user.current_room = room
+        request.user.save()
+
+        return Response(RoomJoinSerializer(room).data)
+    
+class LeaveRoom(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        serializer = RoomLeaveSerializer(data={},context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'message':'left the room'})
+
+class getRoom(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        room = request.user.current_room
+        if not room:
+            return Response({'detail':'no room'})
+        return Response(RoomSerializer(room).data)
